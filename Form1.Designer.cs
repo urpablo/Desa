04e8fb465e3dc950900c676d7ddcc0c2fc50b51b
PABLO MARTIN URBANO PATIÑO PABLO MARTIN <25UR92919826@campus.economicas.uba.ar>
2019-11-11 21:12:19 -0300

    .

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Agregar registro";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generar pedido";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(3, 134);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(321, 208);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(702, 356);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(75, 82);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(385, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Agregar pedido";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView2.Location = new System.Drawing.Point(366, 134);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(345, 208);
+            this.listView2.TabIndex = 11;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Cantidad";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(224, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(457, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(146, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(610, 50);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(597, 88);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(146, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(490, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+
+        //Arma el nuevo pedido
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem lstPedido = new ListViewItem(textBox1.Text);
+            lstPedido.SubItems.Add(textBox3.Text);
+            listView1.Items.Add(lstPedido);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool flag = true;
+            using (StreamWriter sw = new StreamWriter("outtemporal.txt"))
+            {
+                //sw.Write("Id" + "," + "Descripcion" + "," + "Cantidad");
+                //sw.Write("\n");
+
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    sw.Write(item.Text);
+                    for (int i = 1; i < item.SubItems.Count; i++)
+                        sw.Write("," + item.SubItems[i].Text);
+                    sw.Write("\n");
+                }
+
+              
+            }
+
+
+            int var1 = 0;
+            string var2;
+            int var3 = 0;
+
+            int vara = 0;
+            string varb;
+            int varc = 0;
+
+            var records = File
+                      .ReadAllLines("outtemporal.txt")
+                      .Select(record => record.Split(','))
+                      .Select(record => new
+                      {
+                          a1 = Int32.Parse(record[0]),
+                          a2 = record[1],
+                         // a3 = Int32.Parse(record[2])
+                      }).ToList();
+
+            var records2 = File
+                      .ReadAllLines("Stock.txt")
+                      .Select(record => record.Split(','))
+                      .Select(record => new
+                      {
+                          b1 = Int32.Parse(record[0]),
+                          b2 = record[1],
+                          b3 = Int32.Parse(record[2])
+                      }).ToList();
+
+
+            foreach (var record in records)
+            {
+                var1 = record.a1;
+                var2 = record.a2;
+                //var3 = record.a3;
+                foreach (var recordb in records2)
+                {
+                    vara = recordb.b1;
+                    varb = recordb.b2;
+                    varc = recordb.b3;
+
+                    if (vara==var1)
+                    {
+                        if (varc < var3)
+                        {
+                            MessageBox.Show("el producto " + var1 + " no se encuentra en stock");
+                            flag = false;
+                        }
+                    }
+                    
+
+                }
+
+            }
+
+            if (flag == false)
+            {
+                MessageBox.Show("Rehaga el formulario por favor");
+            }
+            else
+            {
+                string n = string.Format("Pedido-{0:yyyy-MM-dd_hh-mm}.txt",DateTime.Now);
+
+                using (StreamWriter sw = new StreamWriter(n))
+                {
+                    
+                    sw.Write(textBox4.Text + "," + textBox2.Text);
+                    sw.Write("\n");
+
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        sw.Write(item.Text);
+                        for (int i = 1; i < item.SubItems.Count; i++)
+                            sw.Write("," + item.SubItems[i].Text);
+                        sw.Write("\n");
+                    }
+
+
+                }
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                int var11 = 0;
+                string var22;
+               // int var33 = 0;
+
+                string file = openFileDialog1.FileName;
+                var records3 = File
+                      .ReadAllLines(file)
+                      .Select(record => record.Split(','))
+                      .Select(record => new
+                      {
+                          a11 = Int32.Parse(record[0]),
+                          a22 = record[1],
+                          //a33 = Int32.Parse(record[2])
+                      }).ToList();
+
+                foreach (var recordc in records3)
+                {
+                    var11 = recordc.a11;
+                    var22 = recordc.a22;
+                    //var33 = recordc.a33;
+
+                    ListViewItem lstPedido2 = new ListViewItem(var11.ToString());
+
+                    lstPedido2.SubItems.Add(var22);
+                    //lstPedido2.SubItems.Add(var33.ToString());
+                    listView2.Items.Add(lstPedido2);
+                    
+
+
+                }
+                listView2.Items.Add("---");
+            }
+
+            
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            using (StreamWriter sw = new StreamWriter(textBox7.Text+".txt"))
+            {
+                sw.Write(textBox5.Text + "," + textBox6.Text + "," + textBox7.Text);
+                sw.Write("\n");
+                sw.Write("---");
+                sw.Write("\n");
+
+                foreach (ListViewItem item in listView2.Items)
+                {
+                    sw.Write(item.Text);
+                    for (int i = 1; i < item.SubItems.Count; i++)
+                        sw.Write("," + item.SubItems[i].Text);
+                    sw.Write("\n");
+                }
+                
+
+
+            }
+
+        }
+    }
+}
